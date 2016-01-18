8dd90e03590f82d45771e8123c7b5b52540fbdbe
Dana Engel <engel.dana.mail@gmail.com>
2016-01-18 10:38:39 +0100

    Add file for problem #4

---
+++ src/Problem4.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+ *  Compilation:  javac Problem4.java
+ *  Execution:    java Problem4
+ *
+ *  Project Euler-Title: Largest palindrome product
+ *
+ *  Solves Problem #4 from Project Euler, which is:
+ *  >  A palindromic number reads the same both ways. 
+ *  >  The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ *  >
+ *  >  Find the largest palindrome made from the product of two 3-digit numbers.
+ *
+ ******************************************************************************/
+
+public class Problem4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
