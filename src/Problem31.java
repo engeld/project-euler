906187a8bd929b683ee0814d672a088c13e3a6f5
Dana Engel <engel.dana.mail@gmail.com>
2016-01-18 11:56:08 +0100

    Initial import

---
+++ src/Problem31.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *  Compilation:  javac Problem31.java
+ *  Execution:    java Problem31
+ *
+ *  Project Euler-Title: Coin sums
+ *
+ *  Solves Problem #31 from Project Euler, which is:
+ *  >  In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+ *  >  1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+ *  >
+ *  >  It is possible to make £2 in the following way:
+ *  >  1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+ *  > 
+ *  >  How many different ways can £2 be made using any number of coins?
+ *
+ ******************************************************************************/
+
+public class Problem31 {
+
+	public static void main(String[] args) {
+		// This calls for advanced counting techniques! Mr Buergler would be proud :)
+		// This seems either like a permutation or a combination problem, with repetition
+		
+		
+	}
+}
